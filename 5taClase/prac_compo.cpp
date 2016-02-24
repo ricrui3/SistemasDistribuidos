98079689ee48f242756b8a548fb4f1935f52cb22
Richard <ricrui3@gmail.com>
2016-02-24 12:29:40 -0600

    cambios no necesarios

--- 5taClase/prac_compo.cpp
+++ 5taClase/prac_compo.cpp
@@ -1,35 +1,41 @@
- #include <iostream>
- #include <stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 #include <vector>
-#include <math.h>
+#include <time.h>
 #include "Coordenada.h"
 #include "PoligonoIrreg.h"
 using namespace std;
 
-
+static int numVec = 100000;
 
 int main( )
 {
-	int numVer = 0, numVec = 10000;
+  int numVer = 0, cinn = 0;
 
-	vector<PoligonoIrreg> polI;
-	polI.resize(10000);
-	cout << polI.size() << endl;
-	for(int i = 0; i < polI.size(); i++){
-		numVer = rand() % 100;
-		for(int j = 0; j < numVer; j++){
-			polI[j].anadeVertice(j,numVer);
-		}
-	 	cout << "vertices anadidos " << polI[i].getNumeroVertices() << endl;
-	}
+  vector<PoligonoIrreg> polI;
+  //polI.resize(numVec);
+  cout << polI.size() << endl;
+  /*polI.size()*/
+  for(int i = 0; i < numVec; i++){
+    polI.push_back(PoligonoIrreg());
+    numVer = rand() % 100;
+    //cout << numVer
+    //cin >> cinn;
+    for(int j = 0; j < numVer; j++){
+      cout <<  "this"<<numVer+1 << endl;
+      cout << polI.size() << endl;
+      polI[j].anadeVertice(j,numVer);
+    }
+    //cout << "vertices anadidos " << polI[i].getNumeroVertices() << endl;
+  }
 
- //poligonoIrreg.imprimeVertices();
-	// poligonoIrreg.anadeVertice(5,4);
-	// poligonoIrreg.anadeVertice(4,1);
-	// poligonoIrreg.anadeVertice(3,2);
-	// poligonoIrreg.anadeVertice(2,3);
-	// poligonoIrreg.anadeVertice(1,5);
+  //poligonoIrreg.imprimeVertices();
+  // poligonoIrreg.anadeVertice(5,4);
+  // poligonoIrreg.anadeVertice(4,1);
+  // poligonoIrreg.anadeVertice(3,2);
+  // poligonoIrreg.anadeVertice(2,3);
+  // poligonoIrreg.anadeVertice(1,5);
 
-	// poligonoIrreg.imprimeVertices();
-	return 0;
+  // poligonoIrreg.imprimeVertices();
+  return 0;
 }
