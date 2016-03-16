47f705393699991ebb6c9b526be7acce18773936
ricrui3 <ricrui3@gmail.com>
2016-03-16 10:26:19 -0600

    renobra programas de .c -> -cpp

--- 2doDepartamental/3raClase/programa10-1.cpp
+++ 2doDepartamental/3raClase/programa10-1.cpp
@@ -13,60 +13,57 @@ using namespace std;
 
 int puerto = 7200;
 
-int main()
-{
-   struct sockaddr_in msg_to_server_addr, client_addr, msg_to_client;
-   socklen_t serlen;
-   int s, res,i;
-   int tam = 20;
-   int array[tam], num[tam];
-   serlen = sizeof(msg_to_client);
-   s = socket(AF_INET, SOCK_DGRAM, 0);
+int main() {
+  struct sockaddr_in msg_to_server_addr, client_addr, msg_to_client;
+  socklen_t serlen;
+  int s, res, i;
+  int tam = 20;
+  int array[tam], num[tam];
+  serlen = sizeof(msg_to_client);
+  s = socket(AF_INET, SOCK_DGRAM, 0);
 
-   //llena el arreglo con los valores
-   for (int i = 0; i < tam - 1; ++i)
-   {
-      array[i] = rand() % 100;
-      cout << array[i] << endl;
-   }
+  // llena el arreglo con los valores
+  for (int i = 0; i < tam - 1; ++i) {
+    array[i] = rand() % 100;
+    cout << array[i] << endl;
+  }
 
-   /* rellena la dirección del servidor */
-   bzero((char *)&msg_to_server_addr, sizeof(msg_to_server_addr));
-   msg_to_server_addr.sin_family = AF_INET;
-   msg_to_server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-   msg_to_server_addr.sin_port = htons(puerto);
+  /* rellena la dirección del servidor */
+  bzero((char *)&msg_to_server_addr, sizeof(msg_to_server_addr));
+  msg_to_server_addr.sin_family = AF_INET;
+  msg_to_server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  msg_to_server_addr.sin_port = htons(puerto);
 
+  /* rellena la direcciòn del cliente*/
+  bzero((char *)&client_addr, sizeof(client_addr));
+  client_addr.sin_family = AF_INET;
+  client_addr.sin_addr.s_addr = INADDR_ANY;
 
-   /* rellena la direcciòn del cliente*/
-   bzero((char *)&client_addr, sizeof(client_addr));
-   client_addr.sin_family = AF_INET;
-   client_addr.sin_addr.s_addr = INADDR_ANY;
-   
-   /*cuando se utiliza por numero de puerto el 0, el sistema se encarga de asignarle uno */
-   client_addr.sin_port = htons(0);
-   bind(s, (struct sockaddr *)&client_addr,sizeof(client_addr));
-   for (int i = 0; i < tam; ++i)
-   {
-      num[i] = array[i];
-   }
-   sendto(s, (char *)num, tam * sizeof(int), 0, (struct sockaddr *) &msg_to_server_addr, sizeof(msg_to_server_addr));
-   
-   /* se bloquea esperando respuesta */
-   recvfrom(s, (char *)&res, sizeof(int), 0, (struct sockaddr* )&msg_to_client, &serlen);
-   unsigned char aux[4];
-   memcpy(aux,&msg_to_client.sin_addr.s_addr, 4);
-   for (i = 0; i < 4; i++)
-   {
-      //aux[i]=ntohs(aux[i]);
-      printf("\nLa posicion %d tiene: %d,", i, aux[i]);
-   }
+  /*cuando se utiliza por numero de puerto el 0, el sistema se encarga de
+   * asignarle uno */
+  client_addr.sin_port = htons(0);
+  bind(s, (struct sockaddr *)&client_addr, sizeof(client_addr));
+  for (int i = 0; i < tam; ++i) {
+    num[i] = array[i];
+  }
+  sendto(s, (char *)num, tam * sizeof(int), 0,
+         (struct sockaddr *)&msg_to_server_addr, sizeof(msg_to_server_addr));
 
-   u_short puer;
-   memcpy(&puer,&msg_to_client.sin_port,2);
-   puer = ntohs(puer);
-   printf("\n El puerto es: %d\n",puer);
-   printf("2 + 5 = %d\n", res);
-   close(s);
-   return 0; 
+  /* se bloquea esperando respuesta */
+  recvfrom(s, (char *)&res, sizeof(int), 0, (struct sockaddr *)&msg_to_client,
+           &serlen);
+  unsigned char aux[4];
+  memcpy(aux, &msg_to_client.sin_addr.s_addr, 4);
+  for (i = 0; i < 4; i++) {
+    // aux[i]=ntohs(aux[i]);
+    printf("\nLa posicion %d tiene: %d,", i, aux[i]);
+  }
+
+  u_short puer;
+  memcpy(&puer, &msg_to_client.sin_port, 2);
+  puer = ntohs(puer);
+  printf("\n El puerto es: %d\n", puer);
+  printf("2 + 5 = %d\n", res);
+  close(s);
+  return 0;
 }
-
--- 2doDepartamental/3raClase/udp.cpp
+++ 2doDepartamental/3raClase/udp.cpp
@@ -12,37 +12,36 @@
 
 using namespace std;
 
-int compara(const void *a,const void *b){
-  return (*(int *)a - *(int *)b);
-}
+int compara(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
 
 int puerto = 7200;
 
-int main(void)
-{
+int main(void) {
   int tam = 20;
-   int num[tam];
-   int s, n;
-   socklen_t clilen;
-   struct sockaddr_in server_addr, msg_to_client_addr;
-   
-   s = socket(AF_INET, SOCK_DGRAM, 0);
-   
-   /* se asigna una direccion al socket del servidor*/
-   bzero((char *)&server_addr, sizeof(server_addr));
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = INADDR_ANY;
-   server_addr.sin_port = htons(puerto);
-   bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr));
-   clilen = sizeof(msg_to_client_addr);
-   while(1) {
-      n = recvfrom(s, (char *)num, tam * sizeof(int), 0, (struct sockaddr * )&msg_to_client_addr, &clilen);
+  int num[tam];
+  int s, n;
+  socklen_t clilen;
+  struct sockaddr_in server_addr, msg_to_client_addr;
 
-      cout << num[0] << num[1] <<endl;
-      /* envía la petición al cliente. La estructura msg_to_client_addr contiene la dirección socket del cliente */
-      qsort(num, tam, sizeof(int), compara);
-      cout << num[0] << num[1] << endl;
-      sendto(s, (char *)num, tam * sizeof(int), 0, (struct sockaddr *)&msg_to_client_addr, clilen);
-   }
+  s = socket(AF_INET, SOCK_DGRAM, 0);
+
+  /* se asigna una direccion al socket del servidor*/
+  bzero((char *)&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(puerto);
+  bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  clilen = sizeof(msg_to_client_addr);
+  while (1) {
+    n = recvfrom(s, (char *)num, tam * sizeof(int), 0,
+                 (struct sockaddr *)&msg_to_client_addr, &clilen);
+
+    cout << num[0] << num[1] << endl;
+    /* envía la petición al cliente. La estructura msg_to_client_addr contiene
+     * la dirección socket del cliente */
+    qsort(num, tam, sizeof(int), compara);
+    cout << num[0] << num[1] << endl;
+    sendto(s, (char *)num, tam * sizeof(int), 0,
+           (struct sockaddr *)&msg_to_client_addr, clilen);
+  }
 }
-
