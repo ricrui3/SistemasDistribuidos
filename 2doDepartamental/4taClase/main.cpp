4ee6c8911d9bf4f948b130c3a3440deb39d47db4
ricrui3 <ricrui3@gmail.com>
2016-03-17 11:19:31 -0600

    Cambia paquete datagrama

--- 2doDepartamental/4taClase/main.cpp
+++ 2doDepartamental/4taClase/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void imprimeDatos(char *datos);
 
 int main(int argc, char const *argv[]) {
-  char a[] = {'2', '3', '7', '8', '9', '1', '2', '5'};
+  char a[] = {'2', '3', '7', '8', '9', '1', '8', '5'};
   char ip[] = "192.168.2.1";
   PaqueteDatagrama paq1(a, sizeof(a), ip, 7200);
   // PaqueteDatagrama paq2(20);
@@ -17,4 +17,4 @@ int main(int argc, char const *argv[]) {
 
   return 0;
 }
-// s
+// cambio
