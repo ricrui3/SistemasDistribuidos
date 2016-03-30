a186e02597acd7372599aba2f9288508c16a552d
ricrui3 <ricrui3@gmail.com>
2016-03-30 07:33:31 -0500

    agrega los programas del profesor y el archivo creado en clase

---
+++ 2doDepartamental/5taClase/SocketDatagrama.cpp
@@ -0,0 +1,52 @@
+#include "SocketDatagrama.h"
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <strings.h>
+#include <iostream>
+#include <errno.h>
+
+using namespace std;
+
+SocketDatagrama::SocketDatagrama(int puertoL)
+{
+   s = socket(AF_INET, SOCK_DGRAM, 0);
+   bzero((char *)&direccionLocal, sizeof(struct sockaddr_in));
+   bzero((char *)&direccionForanea, sizeof(struct sockaddr_in));      
+
+   direccionLocal.sin_family = AF_INET;
+   direccionLocal.sin_addr.s_addr = INADDR_ANY;
+   direccionLocal.sin_port = htons(puertoL);
+   bind(s, (struct sockaddr *)&direccionLocal,sizeof(direccionLocal)); 
+}
+
+SocketDatagrama::~SocketDatagrama()
+{
+   close(s);
+}
+
+//Recibe un paquete tipo datagrama proveniente de este socket
+int SocketDatagrama::recibe(PaqueteDatagrama & p)
+{
+   int retorno;
+   socklen_t clilen; //Ojo no compila si es un tipo int en C
+   clilen = sizeof(direccionForanea);
+   retorno = recvfrom(s, (char *) p.obtieneDatos(), p.obtieneLongitud(), 0, (struct sockaddr *)&direccionForanea, &clilen);
+   p.inicializaPuerto(ntohs(direccionForanea.sin_port));
+   p.inicializaIp(inet_ntoa(direccionForanea.sin_addr));
+   
+   return retorno;
+}
+
+//Envía un paquete tipo datagrama desde este socket
+int SocketDatagrama::envia(PaqueteDatagrama & p)
+{
+   int tam;
+   direccionForanea.sin_family = AF_INET;
+   direccionForanea.sin_addr.s_addr = inet_addr(p.obtieneDireccion());
+   direccionForanea.sin_port = htons(p.obtienePuerto());
+   tam = sendto(s, (char *)p.obtieneDatos(), p.obtieneLongitud(), 0, (struct sockaddr *) &direccionForanea, sizeof(direccionForanea));
+   return tam;
+}
---
+++ 2doDepartamental/5taClase/cliente.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "PaqueteDatagrama.h"
+#include "SocketDatagrama.h"
+#include <iostream>
+
+using namespace std;
+
+int puerto = 7200;
+
+int main(int argc, char const *argv[]) {
+  pid_t pid;
+  int i = 1;
+  int num[2] = {3, 4}, tam, estado;
+  //char ipDir[] = "192.168.0.1";
+  char ipDir[] = "127.0.0.1";
+  cout << ipDir << endl;
+
+  SocketDatagrama sckt(0);
+
+  pid = fork();
+
+  switch (pid) {
+    case -1: {
+      perror("Error en la creacion de hijos");
+      break;
+    }
+    case 0: {
+      PaqueteDatagrama recibe(BUFSIZ);
+      while (true) {
+        cout << "Test" << endl;
+        if ((tam = sckt.recibe(recibe)) != -1) {
+          cout << "Servidor encontrado en la direccion: "
+               << recibe.obtieneDireccion() << endl;
+        }
+      }
+      break;
+    }
+    default: {
+      cout << "Proceso padre" << endl;
+      PaqueteDatagrama envia((char *)num, sizeof(int) * 2, ipDir, puerto);
+      for (i = 1; i < 255; i++) {
+        sckt.envia(envia);
+        sprintf(ipDir, "192.168.0.%d", i);
+        // cout << ipDir << endl;
+        envia.inicializaIp(ipDir);
+      }
+      break;
+    }
+  }
+  return 0;
+}
