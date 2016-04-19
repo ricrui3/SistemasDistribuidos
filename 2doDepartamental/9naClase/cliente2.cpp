216b16408362f652b6c8f5c2e89a2045ce2dfb24
ricrui3 <ricrui3@gmail.com>
2016-04-18 19:52:04 -0500

    Archivos clase 9

--- 2doDepartamental/9naClase/cliente2.cpp
+++ 2doDepartamental/9naClase/cliente2.cpp
@@ -24,22 +24,88 @@ int main(int argc, char const *argv[]) {
 
   int tam;
   int puerto = atoi(argv[2]);
-  messageCS * messCS = new messageCS;
-  messageSC * messSC = new messageSC;
+  int offs = 0;
+  bool final = true;
+  messageCS *messCS = new messageCS;
+  messageSC *messSC = new messageSC;
   SocketDatagrama sckt(0);
-  PaqueteDatagrama envia((char *)&messCS, sizeof(struct messageCS), (char *)argv[1],
-                         puerto);
+  PaqueteDatagrama envia((char *)&messCS, sizeof(struct messageCS),
+                         (char *)argv[1], puerto);
   PaqueteDatagrama recibe(BUFSIZ);
-  
-  messCS->opcode = READ; 
-  messCS->count = 687; 
-  messCS->offset = 0;
-  strcpy(messCS->name, (char*)argv[3]);
-  envia.inicializaDatos((char *)messCS);
-  sckt.envia(envia);
-  cout << "Enviado" << endl;
-  tam = sckt.recibe(recibe);
-  Archivo archivo((char*)argv[3],O_WRONLY | O_TRUNC | O_CREAT, 0666);
-  memcpy(messSC, recibe.obtieneDatos(), tam);
-  archivo.escribe(messSC->data,messSC->count);
+
+  messCS->opcode = READ;
+  messCS->count = BUF_SIZE;
+  messCS->offset = offs;
+  strcpy(messCS->name, (char *)argv[3]);
+
+  Archivo archivo((char *)argv[3], O_WRONLY | O_TRUNC | O_CREAT, 0666);
+  while (final) {
+    envia.inicializaDatos((char *)messCS);
+    sckt.envia(envia);
+    cout << "Enviado" << endl;
+    tam = sckt.recibe(recibe);
+    memcpy(messSC, recibe.obtieneDatos(), tam);
+    offs = offs + messSC->count;
+    messCS->offset = offs;
+    cout << "offs: " << offs << endl;
+    archivo.escribe(messSC->data, messSC->count);
+    cout << "tamanio: " << messSC->count << endl << endl;
+    if (messSC->count < BUF_SIZE) {
+      final = false;
+    }
+  }
+}
+
+void setTimeOutRTO(){
+  for (j = 0; j < 7; j++) {
+      numSolTot++;
+      (*men).secuencia = j + 1;
+      (*men).solicitud[0] = rand() % 100;
+      (*men).solicitud[1] = rand() % 100;
+      (*men).respuesta = 0;
+      envia.inicializaDatos((char *)men);
+      // cout << (*men).solicitud[0] << "." << (*men).solicitud[0] << endl;
+      sckt.envia(envia);
+      gettimeofday(&inicial, NULL);
+      if ((tam = sckt.recibeTimeOut(recibe)) != -1) {
+        gettimeofday(&tiempoFinal, NULL);
+        timersub(&tiempoFinal, &inicial, &tiempoTotal);
+        long double lalala =
+            tiempoTotal.tv_usec / float(1000000) + tiempoTotal.tv_sec;
+        delta = lalala - srtt;
+        srtt = srtt + 0.125 * delta;
+        rttvar = rttvar + 0.25 * (absoluto(delta) - rttvar);
+        RTO = srtt + 4 * rttvar;
+        cout << "RTT Medido: " << ceil(lalala) << "s "
+             << (ceil(lalala) - lalala) * 1000000 << "us delta: " << delta
+             << " srtt: " << srtt << " rttvar " << rttvar << endl;
+        cout << "RTO:  " << floor(RTO) << " micros: " << ceill((RTO-floor(RTO))*1000000)
+             << endl;
+        sckt.setTimeOut(RTO, ceill((RTO-floor(RTO))*1000000));
+        numResp++;
+        memcpy(menAux, recibe.obtieneDatos(), sizeof(struct mensaje));
+        cout << "Servidor encontrado en la direccion: "
+             << recibe.obtieneDireccion() << " la suma de "
+             << (*menAux).solicitud[0] << " + " << (*menAux).solicitud[1]
+             << " es: " << (*menAux).respuesta << endl << endl;
+        break;
+      } else {
+        gettimeofday(&tiempoFinal, NULL);
+        timersub(&tiempoFinal, &inicial, &tiempoTotal);
+        long double lalala =
+            tiempoTotal.tv_usec / float(1000000) + tiempoTotal.tv_sec;
+        delta = lalala - srtt;
+        srtt = srtt + 0.125 * delta;
+        rttvar = rttvar + 0.25 * (absoluto(delta) - rttvar);
+        RTO = srtt + 4 * rttvar;
+        cout << "RTT Medido: " << ceil(lalala) << "s "
+             << (ceil(lalala) - lalala) * 1000000 << "us delta: " << delta
+             << " srtt: " << srtt << " rttvar " << rttvar << endl << endl;
+        
+        cout << "RTO:  " <<  floor(RTO) << " micros: " << ceill((RTO-floor(RTO))*1000000)
+             << endl;
+        sckt.setTimeOut(RTO, ceill((RTO-floor(RTO))*1000000));
+      }
+    }
+    if (j == 7) break;
 }
